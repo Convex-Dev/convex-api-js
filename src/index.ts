c603e8664fc42584407ce0fa24055877f88b66cf
bill <billbsing@gmail.com>
2020-11-12 11:09:24 +0800

    add index file for library

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+ *
+ *
+ *  Main index.ts library
+ *
+ *
+ */
+
+export { Account } from 'Account'
+export { ConvexAPI } from 'ConvexAPI'
+export { ConvexAPIRequestError, ConvexAPIError } from 'Errors'
+export { IAccountInformation } from 'Interfaces'
